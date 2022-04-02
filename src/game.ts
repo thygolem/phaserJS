2c34075e22e4ad739aff64b45c4becf34928173c
thygolem <thygolem@gmail.com>
2022-04-02 13:01:51 +0200

    configuracion para escenas

--- src/game.ts
+++ src/game.ts
@@ -1,30 +1,10 @@
 import 'phaser';
+import Configuracion from './configuracion';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-    }
 
-    create ()
-    {
-        const logo = this.add.image(400, 70, 'logo');
 
-    }
-}
 
-const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
-};
-
-const game = new Phaser.Game(config);
+window.addEventListener('load', () => {
+    const game = new Phaser.Game(Configuracion);
+});
